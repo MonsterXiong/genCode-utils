b9c1d31c3a543c578c6941dd0a4929aefc43abc6
MonsterXiong <942849672@qq.com>
2024-01-16 15:51:31 +0800

    UPDATE

--- src/enum/componentType.js
+++ src/enum/componentType.js
@@ -1,11 +1,17 @@
+const COMPONENT_ENUM = {
+    COMPONENT:'component',
+    ENTRY:'entry'
+}
+
 
 const COMPONENT_CRUD_ENUM={
     QUERY:"query",
     DIALOG:"dialog",
     TABLE:"table",
-    ENTRY:"entry",
+    ENTRY:COMPONENT_ENUM.ENTRY,
 }
 
 module.exports = {
+    COMPONENT_ENUM,
     COMPONENT_CRUD_ENUM
 }
